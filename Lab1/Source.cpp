6b9ce09a988d45564023c4a555c4b49cc4741d04
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2024-02-25 01:32:58 +0200

    Instead std::fstream used std::ifstream and std::ostream

--- Lab1/Source.cpp
+++ Lab1/Source.cpp
@@ -49,7 +49,7 @@ static Genre& GetM(uint32_t GenreKey) { // Використовує індексну таблицю. Спочат
     indexGenresFile.seekg(0, std::ios::beg);
 
     while (true) {
-        if (indexGenresFile.tellg() == 0) break;
+        //if (indexGenresFile.tellg() == 0) break;
         indexGenresFile.read(reinterpret_cast<char*>(&currentIndex), sizeof(IndexGenre));
         if (currentIndex.key == GenreKey) {
             found = true;
@@ -134,26 +134,28 @@ void AddGenre(Genre& newGenre) {
     genresFile.close();
 
     // Оновлюємо індексну таблицю
-    std::fstream indexGenresFile("indexGenresFile.ind", std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
+    std::ifstream indexGenresFileForRead("indexGenresFile.ind", std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
 
-    if (!indexGenresFile.is_open()) {
+    if (!indexGenresFileForRead.is_open()) {
         std::cerr << "Unable to open file indexGenresFile.ind" << std::endl;
         throw std::runtime_error("Unable to open file indexGenresFile.ind");
     }
-    // indexGenresFile.seekg(0, std::ios::end);     з std::ios::ate прапором і без цього позиція буде в кінці
+    indexGenresFileForRead.seekg(0, std::ios::end);
 
     // Отримати розмір файлу (позначка поточної позиції в кінці файлу)
-    std::streampos endFilePos = indexGenresFile.tellg();
+    std::streampos endFilePos = indexGenresFileForRead.tellg();
 
     // Визначити кількість записів у файлі
     uint32_t indexRecordsCount = endFilePos / sizeof(IndexGenre);
 
     // Перейти до останнього запису
-    indexGenresFile.seekg(-static_cast<std::streamoff>(sizeof(IndexGenre)), std::ios::cur);
+    indexGenresFileForRead.seekg(-static_cast<std::streamoff>(sizeof(IndexGenre)), std::ios::cur);
 
     // Прочитати останній запис
     IndexGenre lastIndex;
-    indexGenresFile.read(reinterpret_cast<char*>(&lastIndex), sizeof(IndexGenre));
+    indexGenresFileForRead.read(reinterpret_cast<char*>(&lastIndex), sizeof(IndexGenre));
+
+    indexGenresFileForRead.close();
 
     // Отримати останній ключ
     uint32_t lastKey = indexRecordsCount > 0 ? lastIndex.key : 0;
@@ -166,8 +168,18 @@ void AddGenre(Genre& newGenre) {
     newIndex.key = newKey;
     newIndex.position = newPosition;
 
+    std::ofstream indexGenresFile("indexGenresFile.ind", std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
+
+    if (!indexGenresFile.is_open()) {
+        std::cerr << "Unable to open file indexGenresFile.ind" << std::endl;
+        throw std::runtime_error("Unable to open file indexGenresFile.ind");
+    }
+
     indexGenresFile.seekp(0, std::ios::end);  // Перейти в кінцеву позицію
     indexGenresFile.write(reinterpret_cast<const char*>(&newIndex), sizeof(IndexGenre));
+
+    indexGenresFile.flush();
+    indexGenresFile.close();
 }
 
 bool Read(Genre& record, std::fstream& file, const std::streampos& pos)
@@ -198,13 +210,14 @@ int main()
 
     Genre newGenre = {0, "new Genre1"};
     AddGenre(newGenre);
-    Genre newGenre2 = { 0, "new Genre2" };
+    Genre newGenre2 = { 1, "new Genre2" };
     AddGenre(newGenre2);
 
     // Випробуємо GetM
-    uint32_t genreKeyToFind = 1; // Припустимо, що це ключ жанру, який ви шукаєте
-    GetM(genreKeyToFind);
+    uint32_t genreKeyToFind = 1;
+    Genre res = GetM(genreKeyToFind);
 
+    std::cout << res.key;
 
     return 0;
 }
